3176116d57a131fc2add6f16ecf88d1ca689edee
xincu <1641479226@qq.com>
2019-10-23 15:14:48 +0800

    1.1

--- src/main/java/com/dome.java
+++ src/main/java/com/dome.java
@@ -3,6 +3,7 @@ package com;
 public class dome {
     public static void main(String[] args) {
         System.out.println("my name is tike");
+        System.out.println("my name is tike2");
 
     }
 }
